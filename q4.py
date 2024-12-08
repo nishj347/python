88c782421a844ed2a9151fe704de03afebce675e
nish <144939153+nishj347@users.noreply.github.com>
2024-12-08 16:45:21 +0530

    Create q4.py

---
+++ q4.py
@@ -0,0 +1,5 @@
+# Write a Python program to find the largest of three numbers.
+n1=int(input("enter first number:"))
+n2=int(input("enter second number:"))
+n3=int(input("enter third number:"))
+print(f"max is {max(n1,n2,n3)}")
