ed406223238e4d2bc4bcc7daddd14f579ee54cb2
nish <144939153+nishj347@users.noreply.github.com>
2024-12-08 16:46:41 +0530

    Create q8.py

---
+++ q8.py
@@ -0,0 +1,8 @@
+# Write a Python program to calculate the power of a number using ** operator
+def demo(base,exponent):
+  return base**exponent
+
+base=int(input("enter the number: "))
+exponent=float(input("enter the number:"))
+print("power is ",demo(base,exponent))
+    
