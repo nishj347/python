582c51d3d7fae35b5ecd3f2463f4e880ab34f0f5
nish <144939153+nishj347@users.noreply.github.com>
2024-12-10 20:06:47 +0530

    Create q12.py

---
+++ q12.py
@@ -0,0 +1,8 @@
+# .Write a Python program that uses a loop to find the sum of squares of the first
+# 100 natural numbers.
+
+result=0
+for i in range(0,101):
+    result+=i*i
+    
+print(result)    
