f89d44da911f3fbd0635ce832dc558fcb9fad923
nish <144939153+nishj347@users.noreply.github.com>
2024-12-12 23:36:27 +0530

    Create q17.py

---
+++ q17.py
@@ -0,0 +1,7 @@
+# 16.Write a Python program to remove duplicates from a list.
+def remove(list1,list2):
+    return list1==list2
+  
+list1=[342,32,12,342,12,21,21,21,21,342,32]
+list2=[23,13,2,1,3,112,12]
+print(remove(list1,list2))
