652319e5be92b74b4762d9592f7baaf3253bc2b8
nish <144939153+nishj347@users.noreply.github.com>
2024-12-13 16:51:43 +0530

    Create q19.py

---
+++ q19.py
@@ -0,0 +1,8 @@
+# 19.Write a Python program to merge two lists into one.
+def concatenate(list1,list2):
+    return list1+list2
+    
+  
+list1=[342,32,12,342,12,21,21,21,21,342,32]
+list2=[23,13,2,1,3,112,12,21,21]
+print(concatenate(list1,list2))
